7e881a8d82d439a0d697e6fc769928746257701d
Fabricio Zuardi <fabricio@gmail.com>
2008-11-25 10:35:20 -0800

    First version of the script

---
+++ twitter.simple.autocomplete.user.js
@@ -0,0 +1,111 @@
+// ==UserScript==
+// @name          Autocompletter: Simple Twitter auto-complete.
+// @description	  Cycle through the different tweet authors on the current page as you type Esc on a word beginning with @
+// @namespace     http://www.mamata.com.br/greasemonkey
+// @include       http://twitter.com/*
+// @include       http://www.twitter.com/*
+
+//by Fabricio Zuardi (http://fabricio.org)
+// ==/UserScript==
+(function() {
+    window.addEventListener("load", function(e) {
+        
+        function createAutoCompleteTable(){
+            var table = [];
+            var tweets = document.getElementById('timeline_body').getElementsByTagName('TR');
+            var authors = [];
+            for (var i in tweets){
+                var tweetClassParts = tweets[i].className.split(' ');
+                var tweetAuthor = tweetClassParts[tweetClassParts.length-1];
+                if (!authors[tweetAuthor]) {
+                    authors[tweetAuthor] = true;
+                } else {
+                    continue;
+                }
+                for (var j=0; j<tweetAuthor.length; j++){
+                    var slug = tweetAuthor.substring(0,j).toLowerCase();
+                    if (!table[slug]) table[slug] = [];
+                    table[slug].push(tweetAuthor);
+                }
+            }
+            return table;
+        }
+
+        var autoCompTable = createAutoCompleteTable();
+        var autoCompleteMode = false;
+        var lastTweetLength = 0;
+        var cycleCounter = 0;
+        var optionsNumber = 0;
+
+        document.getElementById("status").addEventListener("keyup", function(event) {
+            var tweetText = event.target.value;
+            var lastTypedChar = tweetText.substring(event.target.selectionStart-1, event.target.selectionStart);
+            var caretPosition = event.target.selectionStart;
+
+            switch (lastTypedChar){
+                case '@':
+                    autoCompleteMode = true;
+                    break;
+                case '':
+                case ' ':
+                case ',':
+                    autoCompleteMode = false;
+                    break;
+            }
+
+            // 27 = Esc key
+            //  8 = Backspace
+            // 46 = Delete
+            if (((tweetText.length == lastTweetLength)&&(event.which != 27))  || event.which == 8 || event.which == 46) {
+                return false;
+            }
+            
+
+            //delete selection
+            event.target.value = tweetText.substring(0, caretPosition) +
+                tweetText.substring(event.target.selectionEnd, tweetText.length);
+            tweetText = event.target.value;
+                        
+            if (autoCompleteMode) {
+                var atStart = tweetText.substring(0, caretPosition).lastIndexOf('@')+1;
+                if (atStart == 0) return false;
+                var slug = tweetText.substring(atStart, caretPosition).toLowerCase();
+                if (slug.indexOf(' ')!=-1) return false;
+                if (slug.length > 0 && autoCompTable[slug]){
+                    optionsNumber = autoCompTable[slug].length;
+                    if (event.which == 27) {
+                        //Esc pressed = cycle through the options
+                        if (cycleCounter+1 < optionsNumber){
+                            cycleCounter++;
+                        } else {
+                            cycleCounter = 0;
+                        }
+                    } else {
+                        cycleCounter = 0;
+                    }
+                    event.target.value = tweetText.substring(0, atStart) +
+                        autoCompTable[slug][cycleCounter] + 
+                        tweetText.substring(atStart + slug.length, tweetText.length);
+                    event.target.selectionStart = atStart + slug.length;
+                    event.target.selectionEnd = atStart + autoCompTable[slug][cycleCounter].length;
+                }
+            }
+
+            lastTweetLength = event.target.value.length;
+        }, true);
+
+        document.getElementById("status").addEventListener("blur", function(event) {
+            if(autoCompleteMode){
+                autoCompleteMode = false;
+                setTimeout(function(){
+                    var maintf = document.getElementById("status");
+                    var tweetText = maintf.value;
+                    var selectionEnd = maintf.selectionEnd
+                    maintf.value = tweetText.substring(0, selectionEnd) + ' ' + tweetText.substring(selectionEnd, tweetText.length)
+                    maintf.selectionStart = maintf.selectionEnd = selectionEnd+1;
+                    maintf.focus();
+                },0);
+            }
+        }, true);
+    }, false);
+})();
